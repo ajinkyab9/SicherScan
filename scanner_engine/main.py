ebd98050d5427cf40a5501ac51d7a1ac9ecd7691
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-07-28 16:42:15 +0530

    feat:(ai_engine): successfully integrated local ollama qwen2.5-coder:7b via httpx. Ai loads the prompt locally, and returns an appropriate response by scanning the code

--- scanner_engine/main.py
+++ scanner_engine/main.py
@@ -1,3 +1,4 @@
+import httpx
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -8,4 +9,20 @@ class CodePayLoad(BaseModel):
 
 @app.post("/scan")
 async def scan_code(payload: CodePayLoad):
-    return {"message": "Success", "code_received": payload.code}
+    llmPrompt = f"Analyse the following code for security vulnerabilities. List any issues found and provide a brief explanation. \n\n{payload.code}"
+
+    async with httpx.AsyncClient() as client:
+       llmResponse = await client.post(
+        "http://localhost:11434/api/generate",
+            json = {
+                "model": "qwen2.5-coder:7b",
+                "prompt": llmPrompt,
+                "stream": False
+            },
+            timeout = 120.0
+        )
+       receivedData = llmResponse.json()
+       print(receivedData)
+    return {
+        "message": "Scan completed", 
+        "vulnerabilitiesFound": receivedData["response"]}       
