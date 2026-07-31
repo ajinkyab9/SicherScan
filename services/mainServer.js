cf78cfbff3e046da312226b87a82d4a5349eec2b
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-07-31 09:39:24 +0530

    fix: added gracefulShutdown after a terminal process was stuck

--- services/mainServer.js
+++ services/mainServer.js
@@ -1,33 +1,54 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-})
+  connectionString: process.env.DATABASE_URL,
+});
 
 const prisma = new PrismaClient({
-    adapter,
-})
+  adapter,
+});
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
-
- app.get("/health", async (req, res) => {
-     try {
-         await prisma.$queryRaw`SELECT 1`;
-         res.json({status: "ok", db: "db connection successful"});
-     } catch (err) {
-         console.log(err);
-         res.status(500).json({status: "error", db: "disconnected"});
-     }
- });
- 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get("/health", async (req, res) => {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+    res.json({ status: "ok", db: "db connection successful" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ status: "error", db: "disconnected" });
+  }
 });
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+async function gracefulShutdown(signal) {
+  console.log(`\n[${signal}] signal received. Shutting down gracefully...`);
+
+  server.close(async () => {
+    console.log("Http server closed. No longer accepting requests.");
+
+    try {
+      await prisma.$disconnect();
+      console.log("Database connection closed successfully.");
+
+      process.exit(0);
+    } catch (err) {
+      console.log("Error during database disconnection:", err);
+
+      process.exit(1);
+    }
+  });
+  server.closeIdleConnections();
+}
+
+process.on("SIGINT", () => gracefulShutdown("SIGINT"));
+process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
