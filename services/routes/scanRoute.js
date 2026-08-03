3314ae993d771d3d1de1783eb8e894230bc161f2
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-03 15:52:38 +0530

    refactor: completed routing layer

--- services/routes/scanRoute.js
+++ services/routes/scanRoute.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import createScanPayload from '../controllers/scanController.js';
+import {
+  createScanPayload,
+  getScanResult,
+} from "../controllers/scanController.js";
 
 const scanRouter = express.Router();
 
-scanRouter.post('/', createScanPayload);
+scanRouter.post("/", createScanPayload);
+scanRouter.get("/:id", getScanResult);
 
 export default scanRouter;
