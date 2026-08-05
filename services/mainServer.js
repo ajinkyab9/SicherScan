f9f9fb02bd88e8312cc6999e35f05021f8ab8bbe
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-05 23:31:40 +0530

    refactor: naming issues and minor bug fixes

--- services/mainServer.js
+++ services/mainServer.js
@@ -2,9 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import scanRouter from "./routes/scanRoute.js";
-import prisma from "./config/db.js"; // Import the shared instance
+import prisma from "./config/db.js";
 
 const app = express();
+app.use((req, res, next) => {
+  console.log(`🚪 FRONT DOOR: ${req.method} request to ${req.url}`);
+  next(); // Pass it to the next function
+});
 app.use(express.json());
 app.use("/api/scans", scanRouter);
 const PORT = process.env.PORT || 5000;
@@ -14,7 +18,7 @@ app.get("/health", async (req, res) => {
     await prisma.$queryRaw`SELECT 1`;
     res.json({ status: "ok", db: "db connection successful" });
   } catch (err) {
-    console.error(err);
+    console.log(err);
     res.status(500).json({ status: "error", db: "disconnected" });
   }
 });
@@ -32,9 +36,11 @@ async function gracefulShutdown(signal) {
     try {
       await prisma.$disconnect();
       console.log("Database connection closed successfully.");
+
       process.exit(0);
     } catch (err) {
-      console.error("Error during database disconnection:", err);
+      console.log("Error during database disconnection:", err);
+
       process.exit(1);
     }
   });
