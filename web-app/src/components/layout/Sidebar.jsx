be1a2433d4cfd41278c9495cc4b5f93cf2a8bc62
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-07 10:03:26 +0530

    base styles added

--- web-app/src/components/layout/Sidebar.jsx
+++ web-app/src/components/layout/Sidebar.jsx
@@ -15,12 +15,30 @@
 //   );
 // }
 
+import { LayoutDashboard, Search, Bug, ClipboardClock } from "lucide-react"
+
 export default function Sidebar() {
     return (
         <aside className="w-72 bg-sidebar-blue text-amber-50 shrink-0 flex flex-col rounded-3xl m-1">
-            <div >
-
+            <div className="h-16 flex items-center justify-center border-b border-slate-300 font-bold text-xl">
+                SiloScan
             </div>
+            <nav className="flex-1 p-4">
+                <ul className="space-y-2">
+                    <li className="p-4 flex gap-2 items-center rounded cursor-pointer hover:bg-slate-300 hover:text-black">
+                        <LayoutDashboard size={20}/>
+                        <span className="font-medium">Dashboard</span></li>
+                    <li className="p-4 flex gap-2 items-center rounded cursor-pointer hover:bg-slate-300 hover:text-black">
+                        <Search size={20}/>
+                        <span>Scans</span></li>
+                    <li className="p-4 flex gap-2 items-center rounded cursor-pointer hover:bg-slate-300 hover:text-black">
+                        <Bug size={20}/>
+                        <span>Reports</span></li>
+                    <li className="p-4 flex gap-2 items-center rounded cursor-pointer hover:bg-slate-300 hover:text-black">
+                        <ClipboardClock size={20}/>
+                        <span>History</span></li>
+                </ul>
+            </nav>
         </aside>
     )
 }
