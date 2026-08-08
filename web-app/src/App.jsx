7e70f13d63c2d7c68e7cfd8c70da3b6f3c897f28
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-08 19:27:08 +0530

    feat: added components

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -3,21 +3,23 @@
 //import viteLogo from './assets/vite.svg'
 //import heroImg from './assets/hero.png'
 import DashboardLayout from './components/layout/DashboardLayout'
+import CodeEditor from './components/features/CodeMirrorIntegration';
+
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 return (
     <DashboardLayout>
-      <div className="bg-white p-8 rounded-lg shadow-sm border border-gray-200">
+      <div className="bg-white w-full p-8 rounded-lg shadow-sm border border-gray-200">
         <h1 className="text-2xl font-bold mb-4">Vulnerability Scanner Active</h1>
         <p className="text-gray-600 mb-8">
           The layout is working! The sidebar and header are locked in place.
         </p>
-        
-        <div className="h-96 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center text-gray-400 bg-gray-50 font-mono">
-          // CodeMirror Editor
+        <div className="border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center text-gray-400 bg-gray-50 font-mono">
+              <CodeEditor />
         </div>
+
       </div>
     </DashboardLayout>
   );
