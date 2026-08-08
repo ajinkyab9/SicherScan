c55a49508c9bf45aa3937d5b3e0c94b7fc2db889
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-08 10:14:22 +0530

    feat: base sidebar and header layout set

--- web-app/src/components/layout/Header.jsx
+++ web-app/src/components/layout/Header.jsx
@@ -1,12 +1,13 @@
-// export default function Header() {
-//   return (
-//     <header className="h-16 bg-white border-b border-gray-200 flex items-center px-6 flex-shrink-0 shadow-sm">
-//       <h2 className="text-xl font-semibold text-gray-800">Workspace</h2>
-//       <div className="ml-auto">
-//         <span className="text-sm font-medium text-gray-600 bg-gray-100 px-3 py-1 rounded-full">
-//           Ajinkya
-//         </span>
-//       </div>
-//     </header>
-//   );
-// }
+
+export default function Header() {
+    return (
+        <header className="h-15 rounded-xl bg-sidebar-blue border-b border-slate-300 m-3 flex items-center px-6 shrink-0 shadow-sm"> 
+          <h2 className="text-xl font-semibold text-amber-50">Scan | Workspace</h2>
+          <div className="ml-auto">
+            <span className="text=sm font-medium text-slate-600 bg-slate-100 px-3 py-1 rounded-full cursor-pointer">
+              User
+            </span>
+          </div>
+        </header>
+    )
+}
--- web-app/src/components/layout/Sidebar.jsx
+++ web-app/src/components/layout/Sidebar.jsx
@@ -1,27 +1,10 @@
-// export default function Sidebar() {
-//   return (
-//     <aside className="w-64 bg-gray-900 text-white flex-shrink-0 flex flex-col">
-//       <div className="h-16 flex items-center justify-center border-b border-gray-800 font-bold text-xl">
-//         Scanner Pro
-//       </div>
-//       <nav className="flex-1 p-4">
-//         <ul className="space-y-2">
-//           <li className="p-2 bg-gray-800 rounded cursor-pointer">Dashboard</li>
-//           <li className="p-2 hover:bg-gray-800 rounded cursor-pointer transition-colors">Scans</li>
-//           <li className="p-2 hover:bg-gray-800 rounded cursor-pointer transition-colors">Settings</li>
-//         </ul>
-//       </nav>
-//     </aside>
-//   );
-// }
-
 import { LayoutDashboard, Search, Bug, ClipboardClock } from "lucide-react"
 
 export default function Sidebar() {
     return (
         <aside className="w-72 bg-sidebar-blue text-amber-50 shrink-0 flex flex-col rounded-3xl m-1">
             <div className="h-16 flex items-center justify-center border-b border-slate-300 font-bold text-xl">
-                SiloScan
+                SicherScan
             </div>
             <nav className="flex-1 p-4">
                 <ul className="space-y-2">
@@ -30,7 +13,7 @@ export default function Sidebar() {
                         <span className="font-medium">Dashboard</span></li>
                     <li className="p-4 flex gap-2 items-center rounded-xl cursor-pointer hover:bg-slate-300 hover:text-black transition-colors duration-300 ease-in-out">
                         <Search size={20}/>
-                        <span>Scans</span></li>
+                        <span>Scan</span></li>
                     <li className="p-4 flex gap-2 items-center rounded-xl cursor-pointer hover:bg-slate-300 hover:text-black transition-colors duration-300 ease-in-out">
                         <Bug size={20}/>
                         <span>Reports</span></li>
