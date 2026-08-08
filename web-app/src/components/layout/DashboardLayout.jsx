9d00a9b4179c7d80bccc79e26e386334a3e6a315
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-08 10:12:16 +0530

    feat: base dashboard layout set

--- web-app/src/components/layout/DashboardLayout.jsx
+++ web-app/src/components/layout/DashboardLayout.jsx
@@ -1,19 +1,19 @@
  import Sidebar from './Sidebar';
-// import Header from './Header';
+ import Header from './Header';
 
 export default function DashboardLayout({ children }) {
   return (
     <div className="flex h-screen w-full overflow-hidden bg-gray-50 text-gray-900">
       
-      {/* Left: Fixed Sidebar */}
+      {/* fixed sidebar*/}
       <Sidebar />
 
-      {/* Right: Content Column */}
+      {/* main content column */}
       <div className="flex-1 flex flex-col">
+        <Header /> 
         {/* Fixed Header */}
 
-        
-        {/* Main Workspace (Handles its own scrolling) */}
+
         <main className="flex-1 overflow-y-auto p-6">
           {children}
         </main>
