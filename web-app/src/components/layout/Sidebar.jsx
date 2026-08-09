abd51e7dcc31eb567826a1d6bb241509e110ba2f
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-09 22:30:47 +0530

    refactor: minor code changes

--- web-app/src/components/layout/Sidebar.jsx
+++ web-app/src/components/layout/Sidebar.jsx
@@ -1,6 +1,15 @@
 import { LayoutDashboard, Search, Bug, ClipboardClock, ChartSpline } from "lucide-react"
 
-export default function Sidebar() {
+export default function Sidebar({ activeTab, setActiveTab }) {
+
+    const getClasses = (tabName) => {
+        const baseClasses = "p-4 flex gap-2 items-center rounded-xl cursor-pointer transition-colors duration-300 ease-in-out";
+        if (activeTab === tabName) {
+            return `${baseClasses} bg-slate-300 text-black`;
+        }
+        return `${baseClasses} hover:bg-slate-300 hover:text-black text-amber-50`;
+    }
+
     return (
         <aside className="w-72 bg-sidebar-blue text-amber-50 shrink-0 flex flex-col rounded-3xl m-1">
             <div className="h-16 flex items-center justify-center border-b border-slate-300 font-bold text-xl">
@@ -8,19 +17,19 @@ export default function Sidebar() {
             </div>
             <nav className="flex-1 p-4">
                 <ul className="space-y-2">
-                    <li className="p-4 flex gap-2 items-center rounded-xl cursor-pointer hover:bg-slate-300 hover:text-black transition-colors duration-300 ease-in-out">
+                    <li onClick={() => setActiveTab('dashboard')} className={getClasses('dashboard')}>
                         <LayoutDashboard size={20}/>
                         <span className="font-medium">Dashboard</span></li>
-                    <li className="p-4 flex gap-2 items-center rounded-xl cursor-pointer hover:bg-slate-300 hover:text-black transition-colors duration-300 ease-in-out">
+                    <li onClick={() => setActiveTab('scan')} className={getClasses('scan')}>
                         <Search size={20}/>
                         <span>Scan</span></li>
-                    <li className="p-4 flex gap-2 items-center rounded-xl cursor-pointer hover:bg-slate-300 hover:text-black transition-colors duration-300 ease-in-out">
+                    <li onClick={() => setActiveTab('reports')} className={getClasses('reports')}>
                         <Bug size={20}/>
                         <span>Reports</span></li>
-                    <li className="p-4 flex gap-2 items-center rounded-xl cursor-pointer hover:bg-slate-300 hover:text-black transition-colors duration-300 ease-in-out">
+                    <li onClick={() => setActiveTab('history')} className={getClasses('history')}>
                         <ClipboardClock size={20}/>
                         <span>History</span></li>
-                    <li className="p-4 flex gap-2 items-center rounded-xl cursor-pointer hover:bg-slate-300 hover:text-black transition-colors duration-300 ease-in-out">
+                    <li onClick={() => setActiveTab('analytics')} className={getClasses('analytics')}>
                        <ChartSpline size={20}/>
                         <span>Analytics</span>
                     </li>
