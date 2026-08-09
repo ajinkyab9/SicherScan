fe4039d4f5e212ee02f964eae230cb572c460d73
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-09 22:26:54 +0530

    feat: main app layout

---
+++ web-app/src/components/layout/MainAppLayout.jsx
@@ -0,0 +1,18 @@
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+
+export default function MainAppLayout({ children, activeTab, setActiveTab }) {
+    return (
+        <div className="flex h-screen w-full overflow-hidden bg-gray-50 text-gray-900"> 
+            <Sidebar activeTab={activeTab} setActiveTab={setActiveTab}/>
+
+            <div className="flex-1 flex flex-col">
+                <Header />
+
+                <main className="flex-1 overflow-y-auto p-6">
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
+}
