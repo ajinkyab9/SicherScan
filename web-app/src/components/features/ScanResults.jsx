97ba1a142ac84b449ba404e29d470f1aad7bb1fe
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-12 22:56:46 +0530

    feat: componenet to process and display recent scan results

---
+++ web-app/src/components/features/ScanResults.jsx
@@ -0,0 +1,9 @@
+export default function CurrentScanResults() {
+    return (
+        <div className="p-20">
+            <div className="mb-10">
+                Scan results pane is under development
+            </div>
+        </div>
+    )
+}
