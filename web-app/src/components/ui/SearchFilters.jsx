0e3342949b39c54c3c8210b37aa90524846bbe9d
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-13 10:19:08 +0530

    feat: main common search filter component

---
+++ web-app/src/components/ui/SearchFilters.jsx
@@ -0,0 +1,13 @@
+function DatePicker() {
+    return (
+        <div class="relative max-w-sm">
+            <input
+                type="date"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Select date"
+            />
+        </div>
+    )
+}
+
+export { DatePicker }
