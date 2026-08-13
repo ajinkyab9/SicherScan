84059f8558d792aafc7f0fe78bb4ade3b1a24046
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-13 10:16:42 +0530

    refactor: added history tab and removed placeholder code

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import MainAppLayout from './components/layout/MainAppLayout';
 import ScanContent from './components/layout/ScanContent';
 import Dashboard from './components/layout/Dashboard';
+import ScanHistory from './components/layout/History';
 import './App.css';
 
 function App() {
@@ -13,20 +14,15 @@ function App() {
         return <Dashboard />
       case "scan":
         return <ScanContent />;
-      case "reports":
-        return (
-          <div className="bg-white p-8 rounded-lg shadow-sm border border-gray-200 h-full">
-            <h1 className="text-2xl font-bold mb-6">Reports</h1>
-            <p>Downloadable Vulnerability reports .</p>
-          </div>
-        );
+      // case "reports":
+      //   return (
+      //     <div className="bg-white p-8 rounded-lg shadow-sm border border-gray-200 h-full">
+      //       <h1 className="text-2xl font-bold mb-6">Reports</h1>
+      //       <p>Downloadable Vulnerability reports .</p>
+      //     </div>
+      //   );
       case "history":
-        return (
-          <div className="bg-white p-8 rounded-lg shadow-sm border border-gray-200 h-full">
-            <h1 className="text-2xl font-bold mb-6">History</h1>
-            <p>The tab for historical data</p>
-          </div>
-        );
+        return <ScanHistory />
       case "analytics":
         return (
           <div className="bg-white p-8 rounded-lg shadow-sm border border-gray-200 h-full">
