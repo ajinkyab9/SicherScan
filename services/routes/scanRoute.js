11d23c60270e6e30217b67528704cbc062db5d2d
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-19 02:05:14 +0530

    refactor: added languages api

--- services/routes/scanRoute.js
+++ services/routes/scanRoute.js
@@ -3,10 +3,12 @@ import {
   createScanPayload,
   getScanResult,
 } from "../controllers/scanController.js";
+import getLanguages from "../controllers/languageController.js";
 
 const scanRouter = express.Router();
 
 scanRouter.post("/", createScanPayload);
+scanRouter.get("/languages", getLanguages);
 scanRouter.get("/:id", getScanResult);
 
 export default scanRouter;
