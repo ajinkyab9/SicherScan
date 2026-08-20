598f6b94c27286029b978de66e57eea465c649c3
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-20 08:17:02 +0530

    feat: historical scans api implemented

--- services/routes/scanRoute.js
+++ services/routes/scanRoute.js
@@ -4,12 +4,13 @@ import {
   getScanResult,
 } from "../controllers/scanController.js";
 import getLanguages from "../controllers/languageController.js";
+import getHistoricalRecords from "../controllers/historyController.js";
 
 const scanRouter = express.Router();
 
 scanRouter.post("/", createScanPayload);
 scanRouter.get("/languages", getLanguages);
-scanRouter.get("/history");
+scanRouter.get("/scanHistory", getHistoricalRecords);
 scanRouter.get("/:id", getScanResult);
 
 export default scanRouter;
