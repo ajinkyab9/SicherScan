66ae5acd4622683db931d8e3e2d02b9334f94cc3
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-20 18:43:14 +0530

    refactor: inclusion of ids

--- services/controllers/historyController.js
+++ services/controllers/historyController.js
@@ -4,16 +4,17 @@ export default async function getHistoricalRecords(req, res) {
   try {
     const historicalRecords = await prisma.scan.findMany({
       select: {
+        id: true,
         codeLang: true,
         createDate: true,
-        status: true, 
+        status: true,
         vulnerabilities: {
           select: {
             vulType: true,
             severity: true,
             cvssBaseScore: true,
-          }
-        }
+          },
+        },
       },
     });
     return res.status(200).json(historicalRecords);
