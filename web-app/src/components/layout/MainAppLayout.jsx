f46fc25415d384dafb35af57c3d8105c7c0751f5
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-22 22:21:58 +0530

    refactor: ui adjustments

--- web-app/src/components/layout/MainAppLayout.jsx
+++ web-app/src/components/layout/MainAppLayout.jsx
@@ -3,13 +3,13 @@ import Header from "./Header";
 
 export default function MainAppLayout({ children, activeTab, setActiveTab }) {
     return (
-        <div className="flex h-screen w-full overflow-hidden bg-gray-50 text-gray-900"> 
+        <div className="flex h-screen w-full overflow-hidden bg-gray-100 text-gray-900"> 
             <Sidebar activeTab={activeTab} setActiveTab={setActiveTab}/>
 
-            <div className="flex-1 flex flex-col">
+            <div className="flex-1 flex  flex-col">
                 <Header activeTab={activeTab}/>
 
-                <main className="flex-1 overflow-y-auto p-6">
+                <main className="flex-1 p-2">
                     {children}
                 </main>
             </div>
