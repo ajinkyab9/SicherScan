8a5eadfa237daf4e073d3ce9bde5768c6b51e330
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-08-22 22:22:57 +0530

    refactor: btn ui changes

--- web-app/src/components/layout/ScanContent.jsx
+++ web-app/src/components/layout/ScanContent.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import AtAGlanceStats from "../ui/AtAGlanceStats";
 import CodeEditor from "../features/CodeMirrorIntegration";
 import CurrentScanResults from "../features/ScanResults";
 
@@ -20,19 +19,34 @@ export default function ScanContent() {
   }
 
   return (
-    <div className="flex flex-col h-full gap-4">
-      <AtAGlanceStats />
+    <div className="flex flex-col gap-4 h-full">
 
-      <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-200 flex-1">
-        <div className="flex flex-row gap-5 align-middle items-center">
+      <div className="bg-white p-4 rounded-lg shadow-sm border border-gray-200 flex-1">
+        <div className="flex flex-row gap-5 align-middle items-center mb-3">
           <button
             onClick={() => setCurrentView("codeEditor")}
-            className="max-w-32 bg-transparent items-center justify-center flex border-2 border-slate-800 shadow-lg hover:bg-slate-800 text-slate-800 hover:text-zinc-100 duration-300 cursor-pointer active:scale-[0.98] rounded-xl p-2 hover:cursor-pointer">Code Editor</button>
+            className={`max-w-32 items-center justify-center flex border-2 border-slate-800 shadow-lg duration-300 cursor-pointer active:scale-[0.98] rounded-xl p-2
+                ${currentView === "codeEditor"
+                ? "bg-slate-800 text-zinc-100"
+                : "bg-transparent text-slate-800 hover:bg-slate-800 hover:text-zinc-100"
+              }
+  `}
+          >
+            Code Editor
+          </button>
 
-          <button onClick={() => { setCurrentView("scanResults") }}
-            className="max-w-32 bg-transparent items-center justify-center flex border-2 border-slate-800 shadow-lg hover:bg-slate-800 text-slate-800 hover:text-zinc-100 duration-300 cursor-pointer active:scale-[0.98] rounded-xl p-2 hover:cursor-pointer">Scan Results</button>
+          <button
+            onClick={() => setCurrentView("scanResults")}
+            className={`max-w-32 items-center justify-center flex border-2 border-slate-800 shadow-lg duration-300 cursor-pointer active:scale-[0.98] rounded-xl p-2
+                ${currentView === "scanResults"
+                ? "bg-slate-800 text-zinc-100"
+                : "bg-transparent text-slate-800 hover:bg-slate-800 hover:text-zinc-100"}`}>
+
+            Scan Results
+          </button>
         </div>
         <div className="flex-1">
+
           {activeComponent}
         </div>
       </div>
